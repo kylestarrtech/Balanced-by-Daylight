2698366cacc34ce095195c22afb08f9aac22f416
Kyle <kyle@kylestarr.tech>
2024-02-01 04:35:43 -0500

    - Replaced dashes in the Credits menu with horizontal rulers instead.

--- public/scripts/balance-checker.js
+++ public/scripts/balance-checker.js
@@ -290,12 +290,12 @@ function UpdateRoleSelectionHeaderUI() {
 function DisplayCredits() {
     GenerateAlertModal(
         "Credits/Contributors",
-        "Many people have made great contributions to Balanced by Daylight. This is one way to spotlight them.<br><br>-<br>" +
-        "<b>SHADERS (Kyle)</b> - Started the project, and leads the development of Balanced by Daylight.<br>-<br>" +
-        "<b>Floliroy</b> - Assisted heavily in improvements to Perk search, import/export, added drag/drop, and SO much more...<br>-<br>" +
-        "<b>S1mmyy</b> - Fixed the drag/drop functionality on Firefox and made the system much easier to work with.<br>-<br>" +
-        "<b>Vivian Sanchez</b> - Began the work on the mobile UI for Balanced by Daylight.<br>-<br>" +
-        "<b>WheatDraws</b> - Created the Balanced by Daylight logo.<br>-<br>" +
+        "Many people have made great contributions to Balanced by Daylight. This is one way to spotlight them.<br><br><hr>" +
+        "<b>SHADERS (Kyle)</b> - Started the project, and leads the development of Balanced by Daylight.<br><hr>" +
+        "<b>Floliroy</b> - Assisted heavily in improvements to Perk search, import/export, added drag/drop, and SO much more...<br><hr>" +
+        "<b>S1mmyy</b> - Fixed the drag/drop functionality on Firefox and made the system much easier to work with.<br><hr>" +
+        "<b>Vivian Sanchez</b> - Began the work on the mobile UI for Balanced by Daylight.<br><hr>" +
+        "<b>WheatDraws</b> - Created the Balanced by Daylight logo.<br><hr>" +
         "<br>" + 
         "This project is open source, and contributions are welcome. If you would like to contribute, please visit the <a target='_blank' href='https://github.com/kylestarrtech/DBD-Balance-Checker'>GitHub</a>."
     );
