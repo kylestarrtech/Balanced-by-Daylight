49aa10ad8a32ac0370f6a8887774f9d1d3ce6ff1
Kyle <kyle@kylestarr.tech>
2024-03-23 19:05:05 -0400

    - Move initial resources requests to separate JS file.

---
+++ public/scripts/get-resources.js
@@ -0,0 +1,165 @@
+var Perks = null;
+var Killers = null;
+var Survivors = null;
+var Maps = null;
+var KillerAddonsList = null;
+var Offerings = null;
+var Items = null;
+
+/* -------------------------------------- */
+/* ------------- GET DATA --------------- */
+/* -------------------------------------- */
+
+function GetBalancings() {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            switch (this.status) {
+                case 200:
+                    BalancePresets = JSON.parse(this.responseText);
+                break;
+                default:
+                    console.error("Error getting balancings: " + this.status);
+            }
+            GetPerks();
+        }
+    }
+    xhttp.open("GET", "Balancings.json", false);
+    xhttp.send();
+}
+
+function GetPerks() {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            switch (this.status) {
+                case 200:
+                    Perks = JSON.parse(this.responseText);
+                break;
+                default:
+                    console.error("Error getting perks: " + this.status);
+            }
+            GetKillers();
+        }
+    }
+    xhttp.open("GET", "Perks/dbdperks.json", false);
+    xhttp.send();
+}
+
+function GetKillers() {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            switch (this.status) {
+                case 200:
+                    Killers = JSON.parse(this.responseText);
+                break;
+                default:
+                    console.error("Error getting killers: " + this.status);
+            }
+            GetSurvivors();
+        }
+    }
+    xhttp.open("GET", "Killers.json", false);
+    xhttp.send();
+}
+
+function GetSurvivors() {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            switch (this.status) {
+                case 200:
+                    Survivors = JSON.parse(this.responseText);
+                    
+                break;
+                default:
+                    console.error("Error getting survivors: " + this.status);
+            
+            }
+            GetMaps();
+        }
+    }
+    xhttp.open("GET", "Survivors.json", false);
+    xhttp.send();
+}
+
+function GetMaps() {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            switch (this.status) {
+                case 200:
+                    Maps = JSON.parse(this.responseText);
+                break;
+                default:
+                    console.error("Error getting maps: " + this.status);
+            }
+            GetAddons();
+        }
+    }
+    xhttp.open("GET", "NewMaps.json", false);
+    xhttp.send();
+}
+
+function GetAddons() {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            switch (this.status) {
+                case 200:
+                    KillerAddonsList = JSON.parse(this.responseText);
+                break;
+                default:
+                    console.error("Error getting addons: " + this.status);
+            }
+            GetItems();
+        }
+    }
+    xhttp.open("GET", "NewAddons.json", false);
+    xhttp.send();
+}
+
+function GetItems() {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            switch (this.status) {
+                case 200:
+                    Items = JSON.parse(this.responseText);
+                break;
+                default:
+                    console.error("Error getting items: " + this.status);
+            }
+            GetOfferings();
+        }
+    }
+    xhttp.open("GET", "Items.json", false);
+    xhttp.send();
+}
+
+function GetOfferings() {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            switch (this.status) {
+                case 200:
+                    Offerings = JSON.parse(this.responseText);
+                break;
+                default:
+                    console.error("Error getting offerings: " + this.status);
+            }
+            GetBalancing();
+        }
+    }
+    xhttp.open("GET", "Offerings.json", false);
+    xhttp.send();
+}
