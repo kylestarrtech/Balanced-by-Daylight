b853c75df15ccc54f5c12bcdedb00477ca5ff67b
Kyle <kyle@kylestarr.tech>
2024-08-07 00:30:48 -0400

    - Initial Commit

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -11,4 +11,8 @@ module.exports = function(app) {
     app.get('/balance-checker', (req, res) => {
         res.render("balance-checker.pug");
     })
+
+    app.get('/balance-creator-v2', (req, res) => {
+        res.render("new-balance-creator.pug");
+    })
 }
